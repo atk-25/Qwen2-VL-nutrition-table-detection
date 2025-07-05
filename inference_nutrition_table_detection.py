f79610c7a02c3dbbd5cfbee652022f3911d2ce97
Afewerki Kahsai <113409490+atk-25@users.noreply.github.com>
2025-07-05 08:52:43 -0700

    Update inference_nutrition_table_detection.py

--- inference_nutrition_table_detection.py
+++ inference_nutrition_table_detection.py
@@ -119,11 +119,12 @@ if __name__ == "__main__":
     vision_process.MAX_PIXELS = max_pixels
 
     system_message = "You are a Vision Language Model specialized in interpreting visual data from product images. " \
-                     "Your task is to analyze the provided product images and detect the nutrition tables, delivering coordinates " \
-                     "in bbox format {'bbox_2d': [x1, y1, x2, y2], 'label': 'category name'}. Avoid additional explanation unless absolutely necessary."
-
-    prompt = "Detect the bounding boxes of the nutrition tables present in the image. " \
-             "Output format for each detected table should be as follows: {'bbox_2d': [x1, y1, x2, y2], 'label': 'category name'}."
+                     "Your task is to analyze the provided product images and detect the nutrition tables in a certain format. " \
+                     "Focus on delivering accurate, succinct answers based on the visual information. Avoid additional explanation unless absolutely necessary."
+    
+    prompt = "Detect the bounding boxes of the different categories of nutrition tables present in the image. " \
+             "The possible categories are ['nutrition-table', 'nutrition-table-small', 'nutrition-table-text', 'nutrition-table-small-energy']. " \
+             "Deliver coordinates in bbox format {'bbox_2d': [x1, y1, x2, y2], 'label': 'category name'}."
 
     image = Image.open(args.image_url)
 
