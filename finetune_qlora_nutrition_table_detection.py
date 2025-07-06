ecdebe46c0b665efa2701723e63140752c1724da
Afewerki Kahsai <113409490+atk-25@users.noreply.github.com>
2025-07-06 02:07:25 -0700

    Update finetune_qlora_nutrition_table_detection.py

--- finetune_qlora_nutrition_table_detection.py
+++ finetune_qlora_nutrition_table_detection.py
@@ -313,8 +313,7 @@ if __name__ == "__main__":
                      "Your task is to analyze the provided product images and detect the nutrition tables in a certain format. " \
                      "Focus on delivering accurate, succinct answers based on the visual information. Avoid additional explanation unless absolutely necessary."
     
-    prompt = "Detect the bounding boxes of the different categories of nutrition tables present in the image. " \
-             "The possible categories are ['nutrition-table', 'nutrition-table-small', 'nutrition-table-text', 'nutrition-table-small-energy']. " \
+    prompt = "Detect the bounding boxes of nutrition tables present in the image. " \
              "Deliver coordinates in bbox format {'bbox_2d': [x1, y1, x2, y2], 'label': 'category name'}."
 
     train_ds = [create_conversation_template_for_training(example, system_message=system_message, prompt=prompt) for example in train_ds_raw]
